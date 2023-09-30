5b5e8c318970ad4117dbcf81ef55481e30abd936
Utente <utente@MacBook-di-Utente.local>
2023-09-30 02:35:24 +0200

    An interactive form details with React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormDetails from "./components/FormDetails";
+function App() {
+  return (
+    <div>
+      <FormDetails />
+    </div>
+  )
+}
+
+export default App;
